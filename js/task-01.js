3bfed5f05e81d399934580ae74ac7a82be656a7c
YuliiaSliepchenko <slepajuli@gmail.com>
2023-02-17 00:13:45 +0200

    change task

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const navElCategory = document.querySelectorAll('li.item');
+console.log('Number of categories:', navElCategory.length);
+
+const animalEl = document.querySelectorAll
+('.item,elem.firstElementChild,#categories');
+console.log('Category: Animals Elements:',animalEl.length);
+
+const prodactEl = categories.querySelectorAll
+('.item,elem.secondElementChild,#categories');
+console.log('Category: Prodacts Elements:',prodactEl.length);
+
+const technologiesEl = categories.querySelectorAll
+('ul,#categories,li.item,elem.lastElementChild');
+console.log('Category: Technologies Elements:',technologiesEl.length);
+
