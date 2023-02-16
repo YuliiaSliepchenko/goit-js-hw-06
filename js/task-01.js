1ae1cf0f487153e105d65265c4870eff09617ee0
YuliiaSliepchenko <slepajuli@gmail.com>
2023-02-17 01:04:00 +0200

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,18 @@
-const navElCategory = document.querySelectorAll('li.item');
-console.log('Number of categories:', navElCategory.length);
+const navElCategory = document.querySelectorAll('.item');
+console.log('Number of categories:');
+console.log(navElCategory.length);
 
 const animalEl = document.querySelectorAll
 ('.item,elem.firstElementChild,#categories');
-console.log('Category: Animals Elements:',animalEl.length);
+console.log('Category: Animals Elements:');
+console.log(animalEl.length);
 
 const prodactEl = categories.querySelectorAll
 ('.item,elem.secondElementChild,#categories');
-console.log('Category: Prodacts Elements:',prodactEl.length);
+console.log('Category: Prodacts Elements:');
+console.log(prodactEl.length);
 
 const technologiesEl = categories.querySelectorAll
-('ul,#categories,li.item,elem.lastElementChild');
-console.log('Category: Technologies Elements:',technologiesEl.length);
-
+('li.item,elem.lastElementChild,#categories');
+console.log('Category: Technologies Elements:');
+console.log(technologiesEl.length);
