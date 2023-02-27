eb7c15bd7f8a9a6bfb0f12a7645b91b25388e299
YuliiaSliepchenko <slepajuli@gmail.com>
2023-02-27 21:26:30 +0200

    change code10

--- js/task-10.js
+++ js/task-10.js
@@ -19,37 +19,37 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const refs = {
-  input: document.querySelector("#controls input"),
-  createBtn: document.querySelector("button[data-create]"),
-  destroyBtn: document.querySelector("button[data-destroy]"),
-  boxes: document.querySelector("#boxes"),
-};
+// const refs = {
+//   input: document.querySelector("#controls input"),
+//   createBtn: document.querySelector("button[data-create]"),
+//   destroyBtn: document.querySelector("button[data-destroy]"),
+//   boxes: document.querySelector("#boxes"),
+// };
 
-const createBoxes = (amount) => {
-  const addElements = [];
+// const createBoxes = (amount) => {
+//   const addElements = [];
 
-  for (let i = 0; i < amount; i++) {
-    const divEl = document.createElement("div");
-    divEl.style.width =`${30 + 10 * i}px`;
-    divEl.style.height =`${30 + 10 * i}px`;
-    divEl.style.background = getRandomHexColor();
-    addElements.push(divEl);
-  };
-  refs.boxes.append(...addElements);
-};
+//   for (let i = 0; i < amount; i++) {
+//     const divEl = document.createElement("div");
+//     divEl.style.width =`${30 + 10 * i}px`;
+//     divEl.style.height =`${30 + 10 * i}px`;
+//     divEl.style.background = getRandomHexColor();
+//     addElements.push(divEl);
+//   };
+//   refs.boxes.append(...addElements);
+// };
 
-const getInputValue = () => {
-  const amount = refs.input.value;
-  createBoxes(amount);
-  refs.input.value = "";
-};
-refs.createBtn.addEventListener('click' ,getInputValue );
+// const getInputValue = () => {
+//   const amount = refs.input.value;
+//   createBoxes(amount);
+//   refs.input.value = "";
+// };
+// refs.createBtn.addEventListener('click' ,getInputValue );
 
-const destroyBoxes = () => {
-  refs.boxes.innerHTML = "";
-}
- refs.destroyBtn.addEventListener('click', destroyBoxes);
+// const destroyBoxes = () => {
+//   refs.boxes.innerHTML = "";
+// }
+//  refs.destroyBtn.addEventListener('click', destroyBoxes);
 
 
 
@@ -58,43 +58,45 @@ const destroyBoxes = () => {
 
  
 
-// const controlEl = document.querySelector("#controls")
-// const createEl = document.querySelector("[data-create]")
-// const boxesEl = document.querySelector("#boxes")
-// const destroyEl = document.querySelector("[data-destroy]")
+const controlEl = document.querySelector("#controls")
+const createEl = document.querySelector("[data-create]")
+const boxesEl = document.querySelector("#boxes")
+const destroyEl = document.querySelector("[data-destroy]")
 
-// function createBoxes(amount) {
-//   let markup = ""
-//   for (let i = 0; i < amount; i += 1) {
-//     const size = 30+i*10
-//     markup += `<div style =
-//     "width: ${size}px; height:${size}px;
-//      background-color: ${getRandomHexColor()}"></div>`
-//   }
-//   boxesEl.style.cssText = 'display:flex; justify-content:flex-start;';
-//   boxesEl.innerHTML = ""
-//   boxesEl.insertAdjacentHTML("beforeend", markup)
+function createBoxes(amount) {
+  let markup = ""
+  for (let i = 0; i < amount; i += 1) {
+    const size = 30+i*10
+    markup += `<div style =
+    "width: ${size}px; height:${size}px;
+     background-color: ${getRandomHexColor()}"></div>`
+  }
+  boxesEl.style.cssText = 'display:flex; justify-content:flex-start;';
+  boxesEl.innerHTML = ""
+  boxesEl.insertAdjacentHTML("beforeend", markup)
   
-// }
+}
 
-// createEl.addEventListener("click", handleCreate)
-// function handleCreate() {
-//   const amount = Number(controlEl.firstElementChild.value)
-//   if (amount) {
-//     createBoxes(amount)
-//   }
+createEl.addEventListener("click", handleCreate)
+function handleCreate() {
+  const amount = Number(controlEl.firstElementChild.value)
+  if (amount) {
+    createBoxes(amount)
+  }
+console.log("test");
+}
+destroyEl.addEventListener("click", handleDestroy)
+function handleDestroy() {
+  debugger;
+ boxesEl.innerHTML = "";
+ let getValue= document.getElementsByTagName("input");
+ console.log(getValue);
+ 
+     if (getValue[0].value !="") {
+      getValue[0].value = "";
 
-// }
-// destroyEl.addEventListener("click", handleDestroy)
-// function handleDestroy() {
-//  boxesEl.innerHTML = "",
-//  document.getElementById("controls").reset();
-// }
-
-
-
-
-
+    }
+}
 
 
 // input.value = function clearInput() {
